0a8796fb5de5fa979a47b5d4d9cdcc9a8ad5658c
Mahãyana Vitória <84738174+MahayanaVitoria@users.noreply.github.com>
2022-03-22 21:59:29 -0300

    Add files via upload

---
+++ Aula04/ExemploFuncoes/src/ExemploFuncoes.java
@@ -0,0 +1,26 @@
+
+public class ExemploFuncoes {
+	
+	public static void imprimir(String txt) {
+		System.out.println(txt);
+	}
+	
+	public static double somar(double a, double b) {
+		//mudou o retorno de void para double
+		double vlr = a + b;
+		return vlr;
+	}
+
+	public static void main(String[] args) {
+		//static = função do java
+		// () entrada de valor na função
+		double valor1 = 100;
+		double valor2 = 80;
+		//double resultado = valor1 + valor2;
+		double resultado = somar(valor1, valor2);
+		//System.out.println();
+	
+	String texto = String.valueOf(resultado);
+	imprimir(texto);
+	}
+}
